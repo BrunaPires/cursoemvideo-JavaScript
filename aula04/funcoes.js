aeecb9d7918aa4161c690bd4871059744b943eab
BrunaPires <b.serip@gmail.com>
2020-12-18 17:58:15 -0300

    NewCommit04

---
+++ aula04/funcoes.js
@@ -0,0 +1,6 @@
+    window.alert('Minha primeira mensagem') // mensagem de alerta
+    window.confirm('Está gostando do JS?') // mensagem de confirmação
+    window.prompt('Qual é o seu nome?') // janela de digitação
+
+
+    
---
+++ aula06/funcoes.js
@@ -0,0 +1,9 @@
+
+var nome = window.prompt('Qual é o seu nome?') // janela de digitação
+window.alert('É uma grande prazer em te conhecer, ' + nome + '!') // concatenação
+
+
+
+
+
+    
